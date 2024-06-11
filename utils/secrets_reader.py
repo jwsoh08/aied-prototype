e14978873ff11494581efdd0d3aecc189cc38a74
jwsoh07 <mail.junwen@gmail.com>
2024-06-11 11:26:11 +0800

    Update secrets ref

--- utils/secrets_reader.py
+++ utils/secrets_reader.py
@@ -19,7 +19,7 @@ load_dotenv()
 class SecretsManager:
     @staticmethod
     def get_secret(key):
-        secret_name = "cotf/ai/metacog"
+        secret_name = "cotf/ai/aied"
         region_name = "ap-southeast-1"
 
         # Create a Secrets Manager client
